8d831753482a0b682c72ae2ce5549f1234ca9dd8
Thomas Martens <thomas@defaultschuldiger.de>
2015-07-01 23:26:54 +0200

    added GPLv3 header to libcdim.* and added licensefile

--- libcdim.cpp
+++ libcdim.cpp
@@ -1,3 +1,22 @@
+/*
+ *   libcdim - a library for manipulation CBM imagefiles (mainly d64)
+ * 
+ *   Copyright (C) [2015]  [Thomas Martens]
+ *
+ *   This program is free software; you can redistribute it and/or modify it
+ *   under the terms of the GNU General Public License as published by the
+ *   Free Software Foundation; either version 3 of the License, or (at your
+ *   option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful, but
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *   for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "libcdim.h"
 
 using namespace std;
@@ -315,7 +334,7 @@ namespace cdim
 			direntry.rel_recordlength = *diskdirentry_it;
 			diskdirentry_it++;
 		    
-			/* skip unused bytes (except GEOS discs */
+			/* skip unused bytes (except GEOS discs) */
 			diskdirentry_it = diskdirentry_it + 6;
 		    
 			/* filesize */
--- libcdim.h
+++ libcdim.h
@@ -1,3 +1,22 @@
+/*
+ *   libcdim - a library for manipulation CBM imagefiles (mainly d64)
+ * 
+ *   Copyright (C) [2015]  [Thomas Martens]
+ *
+ *   This program is free software; you can redistribute it and/or modify it
+ *   under the terms of the GNU General Public License as published by the
+ *   Free Software Foundation; either version 3 of the License, or (at your
+ *   option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful, but
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *   for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __CDIM_H
 #define __CDIM_H
 
@@ -11,7 +30,7 @@
 #include <iostream>
 #include <bitset>
 
-/* some usally imagepositions (in decimal)*/
+/* some usally imagepositions (in decimal) */
 #define c_TrackBAM	18
 #define c_SectorBAM	0
 #define c_BitmapBAM	4
@@ -181,4 +200,3 @@ namespace cdim
 }
 
 #endif
-
